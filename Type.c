3dd79be10ebcfaeb41ad798d607e50eee46a4af4
Devesh Mahajan <43674780+deveshm18@users.noreply.github.com>
2019-10-01 13:21:36 +0530

    Update Type.c

--- Type.c
+++ Type.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+long int multiplyNumbers(int n);
 int main()
 {
-	int n;
-	scanf("%d",&n);
-	int factorial,i;
-	factorial=1;
-	for(i=1;i<=n;i++)
-	{
-	factorial*=i;
-
-	}
-
-	printf("%d\n",factorial);
-
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    printf("Factorial of %d = %ld", n, multiplyNumbers(n));
+    return 0;
+}
+long int multiplyNumbers(int n)
+{
+    if (n >= 1)
+        return n*multiplyNumbers(n-1);
+    else
+        return 1;
 }
