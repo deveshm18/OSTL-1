2690a1ee23b1a7a6fe90283199762d23c6089fe4
unknown <leander.maben@gmail.com>
2020-10-01 21:17:31 +0530

    Added week4-8

---
+++ week5/funcs.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int g1(int x){
+	printf("Inside g1(x=%d)\n",x);
+	if(x>0){
+		return 5;
+	}
+	return -1;
+}
+int g2(int x)
+{
+	printf("Inside g2(x=%d)\n",x);
+	int y=g1(x-10);
+	printf("y=%d\n\n",y);
+	return y;
+}
